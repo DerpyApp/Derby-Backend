824e6935c81f602e8f165a2fdfb0a39c43041cf5
Mahmoud Mostafa <106101861+S-han8@users.noreply.github.com>
2026-08-10 00:48:09 +0300

    Update IGenericRepo.cs

--- PadelBooking.DAL/Repositiory/GenericRepo/IGenericRepo.cs
+++ PadelBooking.DAL/Repositiory/GenericRepo/IGenericRepo.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PadelBooking.DAL.Repositiory.GenericRepo
 {
-    internal interface IGenericRepo
+    public interface IGenericRepo<T> where T : class
     {
+        Task<T?> GetByIdAsync(int id); // بتجيب عنصر واحد عن طريق الـ Id.
+        Task<IEnumerable<T>> GetAllAsync(); // بتجيب كل العناصر من الجدول.
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate); // بتجيب كل العناصر اللي بتحقق شرط معين.
+        Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate); // بتجيب أول عنصر يحقق شرط معين أو null لو مفيش.
+        Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate); // بتتحقق إذا كان فيه عنصر يحقق شرط معين ولا لأ.
+        Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null); // بتعد عدد العناصر اللي بتحقق شرط معين أو كل العناصر لو مفيش شرط.
+        Task AddAsync (T entity); // بتضيف عنصر جديد للجدول.
+        Task AddRangeAsync(IEnumerable<T> entities); // بتضيف مجموعة عناصر جديدة للجدول.
+        Task UpdateAsync(T entity); // بتحدث عنصر موجود في الجدول.
+        Task DeleteAsync(T entity); // بتشيل عنصر موجود في الجدول.
+        Task DeleteRangeAsync(IEnumerable<T> entities); // بتشيل مجموعة عناصر موجودة في الجدول.
+
     }
 }
