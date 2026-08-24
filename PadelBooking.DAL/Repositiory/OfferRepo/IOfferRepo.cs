51c2c59eaaa1012de941f1daec189f47b9f8acb8
Ezzatkarem <ztkarmabwmtr@gmail.com>
2026-08-24 17:08:50 +0300

    Admin Endpoints Done

---
+++ PadelBooking.DAL/Repositiory/OfferRepo/IOfferRepo.cs
@@ -0,0 +1,14 @@
+﻿using PadelBooking.DAL.Entities;
+using PadelBooking.DAL.Repositiory.GenericRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadelBooking.DAL.Repositiory.OfferRepo
+{
+    public interface IOfferRepo : IGenericRepo<Offer>
+    {
+    }
+}
